3fb1dc98a427a4557411ad1b07bef4bc2e47a87a
Pooria Soltani <pooria.ms@gmail.com>
2018-11-30 22:59:56 +0330

    improved custom validation

--- responsiveWeb2.js
+++ responsiveWeb2.js
@@ -1,7 +1,7 @@
 // -----------------------------------
 // getting DOM objects
 const body = document.body;
-const fullName = document.getElementById('name');
+const name = document.getElementById('name');
 const age = document.getElementById('age');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
@@ -13,28 +13,23 @@ const forms = document.getElementsByTagName('form');
 // definitions
 const validClass = '<i class="fas fa-check-circle"></i>';
 const invalidClass = '<i class="fas fa-times-circle"></i>';
-
-// form validation
-function validation (field, error) {
-    let spanIconRight = field.parentElement.querySelector('span.icon-right');
-    let spanMessageBox = field.parentElement.parentElement.querySelector('span.message-box');
-    // let spanIconRight = field.nextElementSibling;
-    if (!error) {
-        spanIconRight.innerHTML = validClass;
-        spanIconRight.classList.remove('invalid');
-        spanIconRight.classList.add('valid');
-        spanMessageBox.textContent = '';
-        spanMessageBox.classList.remove('invalid')
-    } else {
-        console.log(error);
-        spanIconRight.innerHTML = invalidClass;
-        spanIconRight.classList.remove('valid');
-        spanIconRight.classList.add('invalid');
-        spanMessageBox.textContent = error;
-        spanMessageBox.classList.add('invalid')
+const patterns = {
+    name: {
+        pattern: /^([^0-9]*)$/,
+        message: 'Your name cannot contain numbers.'
+    },
+    email: {
+        pattern: /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i,
+        message: 'Invalid email format.'
+    },
+    password: {
+        pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/,
+        message: 'Min 6 characters, must have 1 upper, 1 lower, 1 number'
     }
-}
+};
 
+// Form validation
+// Constraint Validation API
 let hasError = function (field) {
     // Don't validate submits, buttons, file & reset inputs, and disabled fields
     if (
@@ -62,23 +57,23 @@ let hasError = function (field) {
     }
     // If too short
     if (validity.tooShort) {
-        return `${field} should be at least ${field.getAttribute('minLength')} characters.`;
+        return `${field.name} should be at least ${field.getAttribute('minLength')} characters.`;
     }
     // If too long
     if (validity.tooLong) {
-        return `${field} should be at most ${field.getAttribute('maxLength')} characters.`;
+        return `${field.name} should be at most ${field.getAttribute('maxLength')} characters.`;
     }
     // If number input isn't a number
     if (validity.badInput) return 'Please enter a number.';
     // If a number value doesn't match the step interval
-    if (validity.stepMismatch) return 'Please select a valid value.';
+    if (validity.stepMismatch) return 'Please enter a valid value.';
     // If a number field is over the max
     if (validity.rangeOverflow) {
-        return `Maximum ${field} should be ${field.getAttribute('max')}.`;
+        return `Maximum ${field.name} should be ${field.getAttribute('max')}.`;
     }
     // If a number field is below the min
     if (validity.rangeUnderflow) {
-        return `Minimum ${field} should be ${field.getAttribute('min')}.`;
+        return `Minimum ${field.name} should be ${field.getAttribute('min')}.`;
     }
     // If pattern doesn't match
     if (validity.patternMismatch) return 'Please match the requested format.';
@@ -86,23 +81,52 @@ let hasError = function (field) {
     return 'The value you entered for this field is invalid.';
 };
 
-// -----------------------------------
-// validations happen here
-for (let i = 0; i < forms.length; i++) {
-    forms[i].setAttribute('novalidate', true);
+// Custom validation
+let hasCustomError = function (field) {
+    if (patterns.hasOwnProperty(field.name)) {
+        if (!patterns[field.name].pattern.test(field.value)) {
+            return patterns[field.name].message;
+        }
+    }
+    return;
+};
+
+function validation (field, error, customError) {
+    let iconRight = field.parentElement.querySelector('span.icon-right');
+    let messageBox = field.parentElement.parentElement.querySelector('span.message-box');
+    // let iconRight = field.nextElementSibling;
+    if (!error && !customError) {
+        iconRight.innerHTML = validClass;
+        iconRight.classList.remove('invalid');
+        iconRight.classList.add('valid');
+        messageBox.textContent = '';
+        messageBox.classList.remove('invalid');
+    }
+    if (error || customError) {
+        iconRight.innerHTML = invalidClass;
+        iconRight.classList.remove('valid');
+        iconRight.classList.add('invalid');
+        messageBox.textContent = error || customError;
+        messageBox.classList.add('invalid');
+    }
 }
 
+// -----------------------------------
+// validations happen here
+// for (let i = 0; i < forms.length; i++) {
+//     forms[i].setAttribute('novalidate', true);
+// }
+
 document.addEventListener('blur', function (event) {
-    // validation (event.target, 'J S');
-    let error = hasError(event.target);
-    validation (event.target, error);
-    // if (error) {
-    //     event.target.classList.add('error');
-    // }
+    if (event.target.nodeName == 'INPUT') {
+        let error = hasError(event.target);
+        let customError = hasCustomError(event.target);
+        validation (event.target, error, customError);
+    }
 }, true);
 
-// fullName.addEventListener('blur', () => {
-//     validation (fullName, 'J S');
+// name.addEventListener('blur', () => {
+//     validation (name, 'J S');
 // });
 // age.addEventListener('blur', () => {
 //     validation (age, 'J S');
@@ -137,6 +161,7 @@ togglePass.addEventListener('click', (event) => {
     password.focus();
 });
 
+togglePass.addEventListener('focus', () => password.focus());
 // custom styles for the 'show password' button (eye)
 password.addEventListener('focus', () => togglePass.style.color = '#268BFE');
 password.addEventListener('focusout', () => togglePass.style.color = '#747574');
